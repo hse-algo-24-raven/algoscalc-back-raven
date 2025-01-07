a1e07ba9a8a6e60eadbf41e30ed2738d6aa8b3f7
Алексей Кушнарев <aakushnarev@edu.hse.ru>
2025-01-07 21:35:05 +0500

    Генерация c пропусками

--- src/algorithms/conveyer_task_generator/function.py
+++ src/algorithms/conveyer_task_generator/function.py
@@ -13,40 +13,58 @@ def __validate_params(task_number: int, min_duration: int, max_duration: int) ->
 def generate_tasks(task_number, min_duration=1, max_duration=20):
     __validate_params(task_number, min_duration, max_duration)
     tasks_list = []
-    first_less_second_cnt = random.randint(1, task_number - 2)
-    second_less_first_cnt = random.randint(1, task_number - first_less_second_cnt - 1)
-    equal_cnt = task_number - first_less_second_cnt - second_less_first_cnt
+
+    first_less_second_cnt = random.randint(1, round(task_number * 0.45))
+    equal_cnt = random.randint(1,  round(task_number * 0.2))
+    second_less_first_cnt = task_number - first_less_second_cnt - equal_cnt
+    print(first_less_second_cnt, equal_cnt, second_less_first_cnt)
 
     for task in range(first_less_second_cnt):
-        procces1 = random.randint(min_duration, max_duration - 1)
-        procces2 = random.randint(min_duration + 1, max_duration)
-        if procces1 == procces2:
-            procces1 += 1
-        current_task = [min(procces1, procces2), max(procces1, procces2)]
+        process1 = random.randint(min_duration, max_duration - 1)
+        process2 = random.randint(min_duration + 1, max_duration)
+        if process1 == process2:
+            process1 += 1
+        current_task = [min(process1, process2), max(process1, process2)]
         tasks_list.append(current_task)
 
     for task in range(second_less_first_cnt):
-        procces1 = random.randint(min_duration, max_duration - 1)
-        procces2 = random.randint(min_duration + 1, max_duration)
-        if procces1 == procces2:
-            procces1 += 1
-        current_task = [max(procces1, procces2), min(procces1, procces2)]
+        process1 = random.randint(min_duration, max_duration - 1)
+        process2 = random.randint(min_duration + 1, max_duration)
+        if process1 == process2:
+            process1 += 1
+        current_task = [max(process1, process2), min(process1, process2)]
         tasks_list.append(current_task)
 
     sum_difference = sum(map(lambda x: x[0] - x[1], tasks_list))
     print(sum_difference)
 
+    while sum_difference <= 0:
+        for task_index in range(len(tasks_list)):
+            current_task = tasks_list[task_index]
+            if current_task[0] < current_task[1]:
+                if current_task[1] - current_task[0] > 1:
+                    current_task[1] -= 1
+                    sum_difference += 1
+            if current_task[0] > current_task[1]:
+                    if current_task[1] > 10:
+                        current_task[1] -= 1
+                        sum_difference += 1
+
+                    if current_task[0] < 20:
+                        current_task[0] += 1
+                        sum_difference += 1
+            tasks_list[task_index] = current_task
 
     sum_difference = sum(map(lambda x: x[0] - x[1], tasks_list))
     print(sum_difference)
     for task in range(equal_cnt):
-        procces1 = random.randint(min_duration, max_duration)
+        process1 = random.randint(min_duration, max_duration)
 
-        current_task = [procces1, procces1]
+        current_task = [process1, process1]
         tasks_list.append(current_task)
 
     print(first_less_second_cnt, second_less_first_cnt, equal_cnt)
-    print(tasks_list)
+    return tasks_list
 
 
 def main(task_number: int, min_duration: int, max_duration: int) -> dict[str, Any]:
